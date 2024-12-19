89e3065c906cd3d393a765223a55f74e4ecc49e8
Zach Bernstein <zebernst@gmail.com>
2024-12-18 20:57:54 -0500

    chore: re-work cross-seed config

---
+++ kubernetes/apps/default/cross-seed/app/resources/config.js
@@ -0,0 +1,33 @@
+// Torrent content layout: Original
+// Default Torrent Management Mode: Automatic
+// Default Save Path: /media/downloads/torrents/seeding
+// Incomplete Save Path: /media/downloads/torrents/incomplete
+
+module.exports = {
+  apiKey: process.env.CROSS_SEED_API_KEY,
+  action: "inject",
+  delay: 30,
+  duplicateCategories: false,
+  excludeOlder: "4w",
+  excludeRecentSearch: "2w",
+  flatLinking: false,
+  includeNonVideos: false,
+  includeSingleEpisodes: true,
+  linkCategory: "cross-seed",
+  linkDir: "/media/downloads/qbittorrent/seeding/cross-seed",
+  linkType: "hardlink",
+  matchMode: "partial",
+  outputDir: "/tmp",
+  port: Number(process.env.CROSS_SEED_PORT),
+  qbittorrentUrl: "http://qbittorrent.default.svc.cluster.local",
+  radarr: [`http://radarr.default.svc.cluster.local/?apikey=${process.env.RADARR_API_KEY}`],
+  searchCadence: "3d",
+  skipRecheck: false,
+  sonarr: [`http://sonarr.default.svc.cluster.local/?apikey=${process.env.SONARR_API_KEY}`],
+  torrentDir: "/qbittorrent/qBittorrent/BT_backup",
+  torznab: [
+      2,  // PTP
+      53, // HUNO
+      54, // HDS
+  ].map(i => `http://prowlarr.default.svc.cluster.local/${i}/api?apikey=${process.env.PROWLARR_API_KEY}`),
+};
