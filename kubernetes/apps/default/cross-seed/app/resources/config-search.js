430fb2d62fb506585c65c10196676d7affae21b3
Zach Bernstein <zebernst@gmail.com>
2024-12-20 15:17:04 -0500

    refactor: move sonarr/radarr to downloads ns

--- kubernetes/apps/default/cross-seed/app/resources/config-search.js
+++ kubernetes/apps/default/cross-seed/app/resources/config-search.js
@@ -29,8 +29,8 @@ module.exports = {
   linkDir: "/media/downloads/qbittorrent/seeding/cross-seed",
   linkType: "hardlink",
 
-  radarr: [`http://radarr.default.svc.cluster.local/?apikey=${process.env.RADARR_API_KEY}`],
-  sonarr: [`http://sonarr.default.svc.cluster.local/?apikey=${process.env.SONARR_API_KEY}`],
+  radarr: [`http://radarr.downloads.svc.cluster.local/?apikey=${process.env.RADARR_API_KEY}`],
+  sonarr: [`http://sonarr.downloads.svc.cluster.local/?apikey=${process.env.SONARR_API_KEY}`],
   torznab: [
       2,  // PTP
       12, // TL
