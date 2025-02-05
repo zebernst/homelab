42de850b147167067fa6a7e3189783c64bfa6963
Zach Bernstein <zebernst@gmail.com>
2025-02-05 12:43:03 -0500

    refactor(cross-seed): simplify setup

--- kubernetes/apps/downloads/cross-seed/app/resources/config.js
+++ kubernetes/apps/downloads/cross-seed/app/resources/config.js
@@ -7,12 +7,51 @@ module.exports = {
   apiKey: process.env.CROSS_SEED_API_KEY,
   port: Number(process.env.CROSS_SEED_PORT),
 
+  // Cross-seed
   action: "inject",
   delay: 30,
   includeNonVideos: false,
-  seasonFromEpisodes: 0.98,
   matchMode: "partial",
+  outputDir: "/config/torrents",
+  seasonFromEpisodes: 0.98,
+  useClientTorrents: true,
 
+  // Qbittorrent
+  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local",
+  duplicateCategories: false,
+  skipRecheck: false,
+
+  // Linking
+  linkCategory: "cross-seed",
+  linkDirs: ["/media/downloads/qbittorrent/seeding/cross-seed"],
+  linkType: "hardlink",
+
+  // Connections
+  radarr: [
+    `http://radarr.downloads.svc.cluster.local/?apikey=$${process.env.RADARR_API_KEY}`,
+    `http://radarr-uhd.downloads.svc.cluster.local/?apikey=$${process.env.RADARR_API_KEY}`,
+  ],
+  sonarr: [
+    `http://sonarr.downloads.svc.cluster.local/?apikey=$${process.env.SONARR_API_KEY}`,
+    `http://sonarr-uhd.downloads.svc.cluster.local/?apikey=$${process.env.SONARR_API_KEY}`,
+  ],
+  torznab: [
+    2,  // PTP
+    12, // TL
+    16, // GTN
+    18, // MLK
+    19, // JPTV
+    52, // SP
+    53, // HUNO
+    54, // HDS
+    55, // ABT
+    56, // GTRU
+    57, // OE
+    58, // OT
+    59, // LST
+  ].map(i => `http://prowlarr.downloads.svc.cluster.local/$${i}/api?apikey=$${process.env.PROWLARR_API_KEY}`),
+
+  // Data-based searching
   dataDirs: [
     "/media/books",
     "/media/movies",
@@ -25,40 +64,4 @@ module.exports = {
   searchCadence: "1 day",
   excludeRecentSearch: "3 days",
   excludeOlder: "2 weeks",
-
-  linkCategory: "cross-seed",
-  linkDirs: ["/media/downloads/qbittorrent/seeding/cross-seed"],
-  linkType: "hardlink",
-
-  radarr: [
-    `http://radarr.downloads.svc.cluster.local/?apikey=$${process.env.RADARR_API_KEY}`,
-    `http://radarr-uhd.downloads.svc.cluster.local/?apikey=$${process.env.RADARR_API_KEY}`,
-  ],
-  sonarr: [
-    `http://sonarr.downloads.svc.cluster.local/?apikey=$${process.env.SONARR_API_KEY}`,
-    `http://sonarr-uhd.downloads.svc.cluster.local/?apikey=$${process.env.SONARR_API_KEY}`,
-  ],
-  torznab: [
-      2,  // PTP
-      12, // TL
-      16, // GTN
-      18, // MLK
-      19, // JPTV
-      52, // SP
-      53, // HUNO
-      54, // HDS
-      55, // ABT
-      56, // GTRU
-      57, // OE
-      58, // OT
-      59, // LST
-  ].map(i => `http://prowlarr.downloads.svc.cluster.local/$${i}/api?apikey=$${process.env.PROWLARR_API_KEY}`),
-
-  qbittorrentUrl: "http://qbittorrent.downloads.svc.cluster.local",
-  duplicateCategories: false,
-  skipRecheck: false,
-
-  outputDir: "/config/torrents",
-  useClientTorrents: true,
 };
-
